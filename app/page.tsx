43a919c450f3657688da340bbdda5f2a63655bb2
Juanjo Fernández <juanjo.fernandez@haa.st>
2025-01-18 17:11:11 +0100

    First translation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
-export default function Home() {
+const messages = [
+  new SystemMessage("Translate the following from English into Italian"),
+  new HumanMessage("hi!"),
+];
+
+export default async function Home() {
   const model = new ChatOpenAI({ model: "gpt-4o-mini" });
+  const baseMessageChunk = await model.invoke(messages);
 
-  console.log(model);
+  console.log(baseMessageChunk);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
